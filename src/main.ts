bf8279e63c17bf69b4526f16716960fff34d3b9d
Jefferson Beleño <jefferm.bv@gmail.com>
2022-07-15 09:19:10 -0500

    db connection added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // polyfill for BigInt when use JSON.stringify() 
+  BigInt.prototype["toJSON"] = function () {
+    return this.toString();
+  };
+  const config = new DocumentBuilder()
+    .setTitle('Test')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
